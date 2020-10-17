b71862478b087c24777dd3343308af680619ad23
Louis Suo <louis.suo@myob.com>
2020-10-17 12:47:42 +1100

    add jsonObject, and make jsonNumber support float

--- Main.hs
+++ Main.hs
@@ -4,7 +4,7 @@ import Control.Applicative
 import Data.Char (isSpace, isDigit)
 data JsonValue = JsonNull
                 | JsonBool Bool
-                | JsonNumber Integer
+                | JsonNumber Double
                 | JsonString String
                 | JsonArray [JsonValue]
                 | JsonObject [(String, JsonValue)]
@@ -27,14 +27,16 @@ jsonString :: Parser JsonValue
 jsonString = charP '"' *> f <* charP '"'
     where f = JsonString <$> spanP (/= '"')
 
-jsonNumber :: Parser JsonValue    
-jsonNumber = f <$> notEmpty (spanP isDigit)
-    where f = JsonNumber . read
+jsonNumber :: Parser JsonValue
+jsonNumber = f <$> notEmpty (spanP included)
+    where
+        f = JsonNumber . read
+        included a = isDigit a || (== '.') a
 
 notEmpty :: Parser String -> Parser String
 notEmpty (Parser p) = Parser $ \i -> do
     (i2, rest) <- p i
-    case rest of 
+    case rest of
         [] -> Nothing
         _ -> Just (i2, rest)
 
@@ -46,14 +48,38 @@ jsonArray = JsonArray <$> (charP '[' *> ws *> ele <* ws <* charP ']')
 sepBy :: Parser Char -> Parser JsonValue -> Parser [JsonValue]
 sepBy s cur = (:) <$> cur <*> many (s *> cur ) <|> pure []
 
+pairBy :: Parser [(String, JsonValue)]
+pairBy = (:) <$> pair <*> many (commaSep *> pair)
+    where pair = (,) <$> pairKey <*> jsonValue
+
+pairKey :: Parser String
+pairKey = quoteP *> spanP (/= '"') <* quoteP <* colonP
+
+colonP :: Parser Char
+colonP = ws *> charP ':' <* ws
+
+commaSep :: Parser Char
+commaSep = ws *> charP ',' <* ws
+
+quoteP :: Parser Char
+quoteP  = charP '"'
+
+startBrace :: Parser Char
+startBrace = ws *> charP '{' <* ws
+
+endBrace :: Parser Char
+endBrace = ws *> charP '}' <* ws
+
+
 ws :: Parser String
 ws = spanP isSpace
 
 jsonObject :: Parser JsonValue
-jsonObject = undefined
+jsonObject = JsonObject <$> (startBrace *> pairBy <* endBrace)
+
 
 spanP :: (Char -> Bool) -> Parser String
-spanP f = Parser $ \input -> let (m, rest) = span f input in Just (rest, m) 
+spanP f = Parser $ \input -> let (m, rest) = span f input in Just (rest, m)
 
 charP :: Char -> Parser Char
 charP x = Parser f
@@ -87,7 +113,7 @@ instance Alternative Parser where
     (Parser a1) <|> (Parser a2) = Parser $ \i -> a1 i <|> a2 i
 
 jsonValue :: Parser JsonValue
-jsonValue = jsonNull <|> jsonBool <|> jsonNumber <|> jsonString <|> jsonArray
+jsonValue = jsonNull <|> jsonBool <|> jsonNumber <|> jsonString <|> jsonArray <|> jsonObject
 
 
 main :: IO ()
@@ -101,5 +127,12 @@ main = do
   print $ runParser jsonValue "\"trueab\"c"
   print $ runParser jsonValue "123p"
   print $ runParser jsonNumber "p"
+  print $ runParser jsonNumber "3.14"
   print $ runParser jsonArray "[]"
-  print $ runParser jsonValue "[2, true, false, []]"
+  print $ runParser jsonValue "[2, true, false, [], \"123\"]"
+  print $ runParser jsonValue "[{\"key\": 42}, true, false, [], \"123\"]"
+  print $ runParser jsonObject "{\"key\":\"a\",\"k\":42}"
+  print $ runParser pairKey "\"key\""
+  print $ runParser (charP '{' *> ws *> commaSep <* ws <* charP '}') "{,}"
+  print $ runParser (charP '{' *> ws *> pairKey <* ws <* charP '}') "{\"somekey\"}"
+
